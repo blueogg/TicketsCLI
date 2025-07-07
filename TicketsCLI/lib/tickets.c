df988b538af34e9e260079b658ba689191c6d85b
Blue <victorgalesi2005@gmail.com>
2025-07-07 14:30:17 +0200

    Sistemato formattazione

--- TicketsCLI/lib/tickets.c
+++ TicketsCLI/lib/tickets.c
@@ -21,7 +21,6 @@ return fPtr;
 }
 
 FILE* ticketsCar(ACCESS _ACCESS){
-
 FILE* fPtr = NULL;
 switch(_ACCESS){
 case 1:
@@ -37,19 +36,20 @@ case 3:
     break;
 }
 return fPtr;
-
 }
 
 void controlloBuffer(){
 int a = 0;
 int b;
 while ((b = getchar()) != '\n' && b != EOF) {
-    a = 1;
-}
+
+        a = 1;
+        }
+
 if (a) {
     puts("Input troppo lungo. Operazione annullata");
     menu();
-}
+       }
 }
 
 void menu(){
@@ -59,7 +59,6 @@ void menu(){
     int scelta;
     scanf_s("%i", &scelta);
     switch(scelta){
-
 case 1:
     creaBigliettoPasseggero();
     break;
@@ -77,18 +76,16 @@ case 5:
     break;
     default:
     break;
-}
+        }
     }
 }
 
 
 void creaBigliettoPasseggero(){
-
     bigliettoPasseggero biglietto;
     passeggero _passeggero;
     rotta partenza;
     rotta arrivo;
-
 //INPUT
     printf("\n%s: \n", "Inserisci nome e cognome del passeggero");
     scanf_s("%15s%15s", _passeggero.nome,15, _passeggero.cognome,15);
@@ -99,14 +96,11 @@ void creaBigliettoPasseggero(){
     printf("\n%s:\n", "Inserisci luogo, dataa (dd-mm-yyyy)e ora (hh-mm) dell'arrivo");
     scanf_s("%30s%11s%6s", arrivo.luogo, 30, arrivo.data, 11, arrivo.ora, 6);
     controlloBuffer();
-
 //BIGLIETTO
-
     biglietto.partenza = partenza;
     biglietto.arrivo = arrivo;
     biglietto.prezzo = 15;
     biglietto.utente = _passeggero;
-
 //SCRITTURA BIGLIETTO SU FILE
     int check = 0;
     FILE* fPtr = ticketsPassenger(WRITE);
@@ -117,12 +111,11 @@ void creaBigliettoPasseggero(){
     }else { puts("Errore nella creazione del biglietto"); }
     fclose(fPtr);
     menu();
-
     }
     else {
         puts("Impossibile aprire il file");
         menu();
-    }
+         }
 }
 
 void creaBigliettoMacchina(){
@@ -132,7 +125,6 @@ void creaBigliettoMacchina(){
     bigliettoMacchina biglietto;
     rotta partenza;
     rotta arrivo;
-
 //INPUT
     printf("%s\n", "Quanti passeggeri nell'auto?");
     scanf_s("%i", &a);
@@ -145,20 +137,12 @@ void creaBigliettoMacchina(){
     controlloBuffer();
     printf("\n%s\n", "Inserisci dimensioni auto (m)");
     scanf_s("%i", &dimensioniAuto);
-
-
-
-
-
 //BIGLIETTO
-
     biglietto.partenza = partenza;
     biglietto.arrivo = arrivo;
     biglietto.prezzo = 15 * a + 5; // Numero di passeggeri * 15 + prezzo 5 euro per la macchina.
     biglietto.lunghezza_auto = dimensioniAuto;
     biglietto.head = head;
-
-
 //SCRITTURA BIGLIETTO SU FILE
     int check = 0;
     FILE* fPtr = ticketsCar(WRITE);
@@ -173,19 +157,17 @@ void creaBigliettoMacchina(){
     while (temp) {
         fwrite(&(temp->_passeggero), sizeof(passeggero), 1, fPtr);
         temp = temp->next;
-    }
+                 }
     fclose(fPtr);
     menu();
-    }}else {
+                    }
+    }else{
         puts("Impossibile aprire il file");
         menu();
-    }
-
-
+        }
 }
 
 int contaBigliettiTotali(){
-
 int a = contaBigliettiPasseggero();
 int b = contaBigliettiMacchina();
 return a+b;
@@ -198,7 +180,7 @@ bigliettoPasseggero biglietto;
     if(fPass != NULL){
     while(fread(&biglietto, sizeof(biglietto), 1, fPass)){
         count++;
-    }
+            }
     fclose(fPass);
     }else { puts("Impossibile aprire il file"); }
 
@@ -213,56 +195,46 @@ if(fPtr != NULL){
 while(fread(&temp, sizeof(int), 1, fPtr) == 1){
     count++;
     fseek(fPtr, sizeof(rotta) * 2 + sizeof(int) * 2 + sizeof(passeggero) * temp, SEEK_CUR);
-}
+                }
 }else {puts("Errore nell'apertura del file"); menu();}
 return count;
-
 }
 
 void nomiBigliettiPasseggero(){
-
-
     bigliettoPasseggero biglietto;
     FILE* fPass = ticketsPassenger(READ);
     int a = contaBigliettiPasseggero();
     if(fPass != NULL){
         if(a != 0){
-
         printf("\n%s\n", "Lista dei nomi biglietti passeggero:");
-        for(int i = 0; i < a; i++)
-            {
+        for(int i = 0; i < a; i++){
         fread(&biglietto, sizeof(biglietto), 1, fPass);
         printf("%s%i: %s %s\n", "Utente", i+1, biglietto.utente.nome, biglietto.utente.cognome);
-            }
+                    }
         fclose(fPass);
         } else {puts("Nessun biglietto trovato");}
-
-    }
+                      }
     menu();
 }
 
 node* initListaPasseggeri(int numeroPasseggeri){
-
-const node* head = NULL;
+node* head = NULL;
 aggiungiNodi(&head, numeroPasseggeri);
-
 return head;
 }
 
  void aggiungiNodi(node** head, int numeroNodi) {
-
     passeggero _passeggero;
     for (int i = 0; i < numeroNodi; i++) {
         node* nuovo = malloc(sizeof(node));
         if (nuovo == NULL) {
             printf("Errore di allocazione memoria.\n");
             menu();
-        }
+                            }
     printf("\n%s: \n", "Inserisci nome e cognome del passeggero");
     scanf_s("%15s%15s", _passeggero.nome, 16, _passeggero.cognome, 16);
     controlloBuffer();
     nuovo->_passeggero = _passeggero;
-
     nuovo->next = NULL;
         if (*head == NULL) {
             // La lista è vuota, questo è il primo nodo
@@ -274,7 +246,7 @@ return head;
                 last = last->next;
             }
             last->next = nuovo;
-        }
+                }
     }
     return;
 }
@@ -287,23 +259,18 @@ int a = contaBigliettiMacchina();
 int b = contaBigliettiPasseggero();
 FILE* fPtr = ticketsCar(READ);
 if(fPtr != NULL){
-
     while(fread(&temp, sizeof(int),1, fPtr) == 1){
     fseek(fPtr, sizeof(rotta) * 2, SEEK_CUR);
     fread(&valore, sizeof(int), 1, fPtr);
     totale += valore;
     fseek(fPtr, sizeof(int) + sizeof(passeggero) * temp, SEEK_CUR);
-
     }
     return totale + 15 * contaBigliettiPasseggero();
-}
-else {
-    puts("Errore nell'apertura del file");
-}
+                }
+else { puts("Errore nell'apertura del file"); }
 }
 
 void passeggeriPerLuogoPartenza(){
-
 passeggero _passeggero;
 rotta partenza;
 bigliettoMacchina _bigliettoMacchina;
@@ -325,21 +292,14 @@ check = 0;
         }
         if(check == 1){
             puts("a");
-            } else {
-
-            printf("\n%s\t%s\n", _bigliettoPasseggero.utente.nome, _bigliettoPasseggero.utente.cognome);
+            } else { printf("\n%s\t%s\n", _bigliettoPasseggero.utente.nome, _bigliettoPasseggero.utente.cognome); }
             }
-
-      }
-            fclose(fPtr1);
-
+fclose(fPtr1);
 //Biglietti Macchina
-
 printf("\n%s\n", "Passeggeri biglietto macchina");
     int num = 0;
     FILE* fPtr2 = ticketsCar(READ);
     while(fread(&num, sizeof(int),1, fPtr2) == 1){
-
         fread(&_bigliettoMacchina.partenza, sizeof(rotta), 1, fPtr2);
         check = 0;
         for(int i = 0; i<lunghezza_luogo; i++){
@@ -349,20 +309,16 @@ printf("\n%s\n", "Passeggeri biglietto macchina");
             } else { break; }
         }
         if(check == 1){
-
             } else {
             fseek(fPtr2, sizeof(rotta) + sizeof(int) * 2, SEEK_CUR);
             for(int i = 0; i < num; i++){
 
             fread(&_passeggero, sizeof(passeggero), 1, fPtr2);
             printf("\n%s\t%s\n", _passeggero.nome, _passeggero.cognome);
-            }
-            }
-
-
-
+                                }
+                 }
     }
-    fclose(fPtr2);
+fclose(fPtr2);
 }
 
 int guadagnoPerGiorno(){
